e3cc55a11ef4f4c2278f2bd82fa84f8986f4bd9a
pabagan <pabagan@gmail.com>
2017-02-06 01:19:40 +0100

    Test user and improve update to use mongoose pre triggers

--- routes/users.js
+++ routes/users.js
@@ -13,21 +13,8 @@ router.get('/', function(req, res, next){
   });
 });
 
-// Get User
-router.get('/:id', function(req, res, next){
-  var userId = req.params.id;
-  
-  User.findById(userId, function(err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
 // Create User
-router.post('/new', function(req, res, next){
+router.post('/create', function(req, res, next){
   var user = req.body;
   
   User.create(user, function(err, result){
@@ -35,63 +22,77 @@ router.post('/new', function(req, res, next){
       res.send(err);
     } else {
       res.json({
-        "info": "User creado con éxito",
-        "data": result
-      });
-    }
-  });
-  /*
-  
-  bcrypt.hash(user.password, saltRounds, function(err, hash) {
-    if(err){
-      res.send('Tenemos problemas para encriptar tu contraseña');
-    } else {
-      user.password = hash;
-      // Create user
-    }
-  });
-   */
-});
-
-// Update User
-router.put('/:id', function(req, res, next){
-  var user = req.body;
-  var userId = req.params.id;
-
-  user.updated_at = new Date();
-
-  User.findByIdAndUpdate(userId, user, function(err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json({
-        "info": "User actualizado con éxito",
-        "data": result
-      });
-    }
-  });  
-});
-
-// Delete User
-router.delete('/:id', function(req, res, next){
-  var user = req.body;
-  var userId = req.params.id;
-  
-  User.findByIdAndRemove(userId, function(err, result) {
-    if (err) {
-      res.send(err);
-    } else if (result == null) {
-      res.json({
-        "info": "El user que intentas borrar ya no existe",
-        "data": result
-      });
-    } else {
-      res.json({
-        "info": "User borrado con éxito",
+        "message": "User created!",
         "data": result
       });
     }
   });
 });
+
+router.route('/:id')
+  // Get User
+  .get(function(req, res, next) {
+    var userId = req.params.id;
+    
+    User.findById(userId, function(err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    });
+  })
+
+  // Delete User
+  .delete(function(req, res, next) {
+    var user = req.body;
+    var userId = req.params.id;
+    
+    User.findByIdAndRemove(userId, function(err, result) {
+      if (err) {
+        res.send(err);
+      } else if (result == null) {
+        res.json({
+          "message": "User not exist",
+          "data": result
+        });
+      } else {
+        res.json({
+          "message": "User deleted!",
+          "data": result
+        });
+      }
+    });
+  })
+
+  // Update User
+  .put(function(req, res, next) {
+    var userReq = req.body;
+    var userId = req.params.id;
+
+    userReq.updated_at = new Date();
+
+    // use findById method to have access to mongoose 
+    // pre triggers.
+    User.findById(userId, function(err, user) {
+      if (err)
+          res.send(err);
+
+      for (var arg in userReq){
+        user[arg] = userReq[arg]
+      }
+
+      // save the updated user
+      user.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ 
+          message: 'Book updated!', 
+          data: user
+        });
+      });
+    });
+  });
 
 module.exports = router;
---
+++ test/users.tests.js
@@ -0,0 +1,172 @@
+// Activate test envirtonment
+process.env.NODE_ENV = 'test';
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var assert = require('assert');
+var request = require("request");
+var should = chai.should();
+var expect = chai.expect;
+//var asert = chai.asert;
+var utils = require('../utils');
+var app = require("../app");
+var server = app.listen(0);
+var port = server.address().port;
+
+// Test Configuration
+var MODEL = 'Users'
+var API_URL = 'http://localhost:' + port + '/api/users'
+var Model = require('../models/User');
+
+/**
+ * Dummy create and update data
+ */
+var DEMO_DOCUMENT = {
+  _id: "5892609064909643fb7e5e62",
+  email: "user@gmail.com", 
+  name: "Mr User", 
+  password: "pass",
+  phone: "+34670588330", 
+  user: "user"
+}
+
+var DEMO_DOCUMENT_UPDATE = {
+  email: "user_2@gmail.com", 
+  name: "Mr Updated Username", 
+  password: "otherpass",
+  phone: "+340707070", 
+  user: "admin"
+}
+
+// chay http middleware
+chai.use(chaiHttp);
+
+/**
+ * 
+ * Start testing!! Go ahead!
+ * 
+ */
+
+/**
+ * Check server errors.
+ */
+describe(MODEL + " testing", function() {
+  it("Check if server is delivering with no errors", function(done) {
+    request(API_URL, function(err, response, body) {
+      if (err) done(err);
+      //var payload = JSON.parse(body);
+      //assert.equal(typeof payload, "object");
+      done();
+    });
+  });
+});
+
+/**
+ * Reset test DB collection.
+ */
+describe(MODEL + " collection reset", function() {
+  it("Clean " + MODEL + " collection", function(done) {
+    Model.remove({}, function(err, response, body) {
+      if (err) done(err);
+      
+      Model.find({}, function(err, result){
+        if (err) done(err);
+
+        assert.equal(result.length, 0);
+        done();
+      });
+    });
+  });
+});
+
+/*
+ * CRUD Test via API with chai-http
+ */
+describe(MODEL + " CRUD API test:", function() {
+  // Create
+  it('Create new ' + MODEL + ' with DEMO_DOCUMENT values', (done) => {
+    chai.request(API_URL)
+      .post('/create')
+      .send(DEMO_DOCUMENT)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        
+        // Check all properties are added 
+        // with provided value.
+        for (var property in DEMO_DOCUMENT) {
+          if(property == "password"){
+            utils.bcryptCompare(DEMO_DOCUMENT['password'], res.body.data['password'], function(error, match){
+              if (!match) done(err);        
+            });
+          } else {
+            res.body.data.should.have.property(property).eql(DEMO_DOCUMENT[property]);
+          }
+        }
+
+        done();
+      });
+  });
+
+  // Read
+  it('Read created ' + MODEL, (done) => {
+    chai.request(API_URL)
+      .get('/' + DEMO_DOCUMENT._id)
+      .end((err, res) => {
+        res.should.have.status(200);
+        assert.equal(res.body._id, DEMO_DOCUMENT._id);
+        res.body.should.be.a('object');
+        
+        // Check all properties are updated 
+        // with provided value.
+        for (var property in DEMO_DOCUMENT) {
+          if(property == "password"){
+            utils.bcryptCompare(DEMO_DOCUMENT['password'], res.body['password'], function(error, match){
+              if (!match) done(err);        
+            });
+          } else {
+            res.body.should.have.property(property).eql(DEMO_DOCUMENT[property]);
+          }
+        }
+        done();
+      });
+  });
+
+  // Update
+  it('Update created ' + MODEL + ' with DEMO_DOCUMENT_UPDATE values', (done) => {
+    chai.request(API_URL)
+      .put('/' + DEMO_DOCUMENT._id)
+      .send(DEMO_DOCUMENT_UPDATE)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        
+        // Check all properties are updated 
+        // with provided value.
+        for (var property in DEMO_DOCUMENT_UPDATE) {
+          if(property == "password"){
+            utils.bcryptCompare(DEMO_DOCUMENT_UPDATE[property], res.body.data[property], function(error, match){
+              if (error) done(err);  
+            });
+          } else {
+            res.body.data.should.have.property(property).eql(DEMO_DOCUMENT_UPDATE[property]);
+          }
+        }
+
+        done();
+      });    
+  });
+
+  // Delete
+  it('Delete created ' + MODEL, (done) => {
+    chai.request(API_URL)
+      .delete('/' + DEMO_DOCUMENT._id)
+      .send(DEMO_DOCUMENT_UPDATE)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message').eql('User deleted!');
+        done();
+      });    
+  });
+});
